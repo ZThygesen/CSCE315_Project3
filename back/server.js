47f7d29a3c77b9eb71c96199082573df7d56dfc2
ZThygesen <zsthygesen@gmail.com>
2022-11-02 17:41:22 -0500

    implement posting data with temperature conversion example

--- back/server.js
+++ back/server.js
@@ -7,10 +7,23 @@ const inventory = require("./routes/inventory");
 const menu = require("./routes/menu");
 const orderItems = require("./routes/orderItems");
 
+app.use(express.json());
 app.use("/api/home", home);
 app.use("/api/inventory", inventory);
 app.use("/api/menu", menu);
 app.use("/api/order-items", orderItems);
+
+
+
+function fahrenheitToCelsius(fahrenheit) {
+    return (fahrenheit - 32) * (5 / 9);
+}
+
+app.post("/api/temp", (req, res) => {
+    console.log(req.body);
+    const celsius = fahrenheitToCelsius(req.body.temp);
+    res.json(celsius);
+});
 
 app.listen(port, () => {console.log(`Server started on port ${port}`)});
 
