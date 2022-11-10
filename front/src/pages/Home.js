61b08f8a5df424c961c8d4166c2b79f9ead455aa
Justin Heger <justin.heger@tamu.edu>
2022-11-10 08:22:43 -0600

    Add google maps API to front page

--- front/src/pages/Home.js
+++ front/src/pages/Home.js
@@ -1,8 +1,15 @@
 import { useNavigate } from "react-router-dom";
+import { GoogleMap, Marker, useLoadScript } from '@react-google-maps/api';
+import { useMemo } from "react";
 
 export default function Home() {
     const navigate = useNavigate();
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: "AIzaSyCNNtfx44impW9Ii9Rq26p6D6E05cQNyvk",
+    });
 
+    if (!isLoaded) return <div>Loading...</div>;
+    //return <Map />;
     return (
         <>
             <h1>Welcome to Pom & Honey!</h1>
@@ -11,6 +18,37 @@ export default function Home() {
                 <button onClick={() => navigate("create-order")}>Place an Order</button>
                 <button onClick={() => navigate("employee")}>Employee Login</button>
             </div>
+            <div className="map-title">We are located in the MSC!</div>
+            <Map />
         </>
+        
     );
 }
+
+function Map() {
+    const center = useMemo(() => ({lat: 30.612279124091536, lng: -96.34155326707841}), []); 
+
+    return (
+        <GoogleMap
+                zoom={17} 
+                center={center}
+                mapContainerClassName="map-container"
+            >
+                <Marker position= {center} />
+            </GoogleMap>
+    );
+}
+
+// {/* <h1>We are located in the MSC!</h1>
+//             <LoadScript
+//                 googleMapsApiKey="AIzaSyCNNtfx44impW9Ii9Rq26p6D6E05cQNyvk"
+//             >
+//                 <GoogleMap
+//                     mapContainerStyle={containerStyle}
+//                     center={center}
+//                     zoom={10}
+//                 >
+//           { /* Child components, such as markers, info windows, etc. */ }
+//           <></>
+//         </GoogleMap>
+//       </LoadScript> */}
