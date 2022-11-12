d9ee3f6b854e0b17b604095f4d8c2eabde570bb0
ardian-kuswanto <ardian.kuswanto@tamu.edu>
2022-11-12 17:18:47 -0600

    Implements UpdateMenu, Adds website translation with Google Translation

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import { FaLeaf } from "react-icons/fa";
-import { Helmet } from "react-helmet"
 import "./Header.css";
+import { GoogleTranslate } from "./GoogleTranslate";
 
 export default function Header() {
     const google = window.google
@@ -11,18 +11,9 @@ export default function Header() {
             <Link to="/" id="site-title">
                 <h1><FaLeaf id="leaf-icon" />Pom & Honey</h1>
             </Link>
-
-            {/*<Helmet>
-                <div id="google_translate_element"></div>
-
-                <script type="text/javascript">
-                function googleTranslateElementInit() {
-                new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element')
-                }
-                </script>
-
-                <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-            </Helmet>*/}
+            <GoogleTranslate/>
+            
         </header>
     );
 }
+
--- front/src/pages/manager/MenuRoutes.js
+++ front/src/pages/manager/MenuRoutes.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Menu from "./Menu";
 import AddMenu from "./AddMenu";
 import RemoveMenu from "./RemoveMenu";
+import UpdateMenu  from "./UpdateMenu";
 
 export default function MenuRoutes() {
     return (
@@ -10,6 +11,7 @@ export default function MenuRoutes() {
                 <Route index element={<Menu />} />
                 <Route path="add-menu-item" element={<AddMenu />} />
                 <Route path="remove-menu-item" element={<RemoveMenu />} />
+                <Route path="update-menu-item" element={<UpdateMenu />} />
             </Routes>
         </>
     );
