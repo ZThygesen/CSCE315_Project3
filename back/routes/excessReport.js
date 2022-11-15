009d75a34e59f713dd417e85d2be9d6de859d459
Justin Heger <justin.heger@tamu.edu>
2022-11-15 11:08:54 -0600

    Excess report accepts user input. Still might have an issue with data gathering from the database

--- back/routes/excessReport.js
+++ back/routes/excessReport.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const conn = require("../db");
 
-router.get("/", async(req, res) => {
+router.post("/", async(req, res) => {
     var start = "2022-10-16";
     var end = "2022-12-05"; 
-    console.log(req.body.start + req.body.end);
+    console.log("START: " + req.body.start + " END: " + req.body.end);
     const query = "SELECT inventory.product_name, COUNT(*) AS total_servings, inventory.total_quantity," + 
     "inventory.serving_size FROM inventory, order_product, orders WHERE inventory.product_id = order_product.product_id AND orders.order_id = " +
     "order_product.order_id AND orders.order_date BETWEEN '" + start + "' AND '" + end + 
--- front/src/pages/manager/ExcessReport.js
+++ front/src/pages/manager/ExcessReport.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 var whattest = [];
 export default function ExcessReport() {
@@ -15,32 +15,31 @@ export default function ExcessReport() {
     function handleSubmit() {
         //e.preventDefault();
 
-        // const startDate = document.getElementById("start").value;
-        // const endDate = document.getElementById("end").value;
-        const start = "2022-10-16";
-        const end = "2022-12-05"; 
+        const start = document.getElementById("start").value;
+        const end = document.getElementById("end").value;
+        //var start = '2022-10-16';
+        //var end ='2022-12-05'; 
 
         if(start === '' || end === ''){
             alert("Please enter all the fields")
         } else {
 
             fetch("/api/excessReport", {
-                method: "GET",
+                method: "POST",
                 headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ start: start, end: end})
+                body: JSON.stringify({ start: start, end: end })
             })
                 .then(res => res.json())
                 .then(excessReport => setExcessReport(excessReport.excessReport))
-            
-            alert("Update sent");
         }
     }
 
     function getExcess(item) {
+        whattest = [];
         for (let i = 0; i < item.length; i++) {
             var numItemsSold = Number(item[i].total_servings) * item[i].serving_size;
-            if ((numItemsSold / (item[i].total_quantity+numItemsSold)) < 0.1) {
-                
+            if ((numItemsSold / (item[i].total_quantity + numItemsSold)) < 0.1) {
+                console.log("ITEM INFO: " + item[i].product_name + (numItemsSold / (item[i].total_quantity + numItemsSold)));
                 whattest.push(item[i].product_name);
             }
             
@@ -70,6 +69,10 @@ export default function ExcessReport() {
                 <button onClick={() => {
                     dualFunction();
                 }}> Generate Excess Report </button>
+                <button onClick={() => {
+                    whattest = [];
+                    console.log(whattest);
+                }}> Clear Excess Report </button>
             </div>
             
             <table className="excessReport-table">
@@ -79,15 +82,15 @@ export default function ExcessReport() {
                     </tr>
                 </thead>
                 <tbody>
-                    {/* {
+                    {
                         whattest.map(item => {
                             return (
                                 <tr key = {item.product_name}>
-                                    <td>[item.product_name</td>
+                                    <td>{item}</td>
                                 </tr>
                             );
                         })
-                    } */}
+                    }
                 </tbody>
             </table>
         </div>
