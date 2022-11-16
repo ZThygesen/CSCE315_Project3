dac0130d1e2c236835f90efd03486782371cd716
justin-heger <justin.heger@tamu.edu>
2022-11-16 11:11:18 -0600

    Working on excess report. Moved functions around, temporarily broken

--- front/src/pages/manager/ExcessReport.js
+++ front/src/pages/manager/ExcessReport.js
@@ -5,20 +5,14 @@ export default function ExcessReport() {
 
     const [excessReport, setExcessReport] = useState([{}]);
 
-    // useEffect(() => {
-    //     fetch("/api/excessReport")
-    //         .then(res => res.json())
-    //         .then(excessReport => setExcessReport(excessReport.excessReport));
-            
-    // }, []);
-
-    function handleSubmit() {
-        //e.preventDefault();
+    function handleSubmit(e) {
+        e.preventDefault();
 
         const start = document.getElementById("start").value;
         const end = document.getElementById("end").value;
         //var start = '2022-10-16';
         //var end ='2022-12-05'; 
+        console.log("here")
 
         if(start === '' || end === ''){
             alert("Please enter all the fields")
@@ -30,7 +24,10 @@ export default function ExcessReport() {
                 body: JSON.stringify({ start: start, end: end })
             })
                 .then(res => res.json())
-                .then(excessReport => setExcessReport(excessReport.excessReport))
+                .then(excessReport => {
+                    setExcessReport(excessReport.excessReport);
+                    getExcess(excessReport);  
+                });
         }
     }
 
@@ -48,32 +45,29 @@ export default function ExcessReport() {
         console.log(whattest);
         return whattest;
     }
-    function dualFunction() {
-        handleSubmit();
-        getExcess(excessReport);   
-    }
+
     return (
         <div className="excessReport-container">
-            <h1>Excess Report</h1>
-            <div className="excess-report-input">
-                    <label htmlFor="start">Enter the start date:</label>
-                    <input type="date" id="start" name="start" />
-            </div>
+            <form className="expressReport-form" onSubmit={handleSubmit}>
+                <h1>Excess Report</h1>
+                <div className="excess-report-input">
+                        <label htmlFor="start">Enter the start date:</label>
+                        <input type="date" id="start" name="start" />
+                </div>
 
-            <div className="excess-report-input">
-                <label htmlFor="end">Enter the end date:</label>
-                <input type="date" id="end" name="end" />
-            </div>
+                <div className="excess-report-input">
+                    <label htmlFor="end">Enter the end date:</label>
+                    <input type="date" id="end" name="end" />
+                </div>
 
-            <div className="excess-report-buttons">
-                <button onClick={() => {
-                    dualFunction();
-                }}> Generate Excess Report </button>
-                <button onClick={() => {
-                    whattest = [];
-                    console.log(whattest);
-                }}> Clear Excess Report </button>
-            </div>
+                <div className="excess-report-buttons">
+                    <button type = "submit"> Generate Excess Report </button>
+                    <button type = "button" onClick={() => {
+                        whattest = [];
+                        console.log(whattest);
+                    }}> Clear Excess Report </button>
+                </div>
+            </form>
             
             <table className="excessReport-table">
                 <thead>
